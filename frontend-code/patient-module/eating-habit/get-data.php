44f6ba486f5c8779cd4cd196195495785e010ffa
Justin Bui <buidominhquanhp1999@gmail.com>
2024-10-09 05:48:56 +1030

    [Eating Habit] [Fix DB search and get tempo]

--- frontend-code/patient-module/eating-habit/get-data.php
+++ frontend-code/patient-module/eating-habit/get-data.php
@@ -6,12 +6,12 @@ $data = [];
 
 // Based on the type
 if ($type === 'exercise') {
-    $result = $conn->query("SELECT * FROM exercises");
+    $result = $conn->query("SELECT * FROM Test.exercises");
     if ($result->num_rows > 0) {
         $data = $result->fetch_all(MYSQLI_ASSOC);
     }
 } elseif ($type === 'food') {
-    $result = $conn->query("SELECT * FROM food_info");
+    $result = $conn->query("SELECT * FROM Test.food_info");
     if ($result->num_rows > 0) {
         $data = $result->fetch_all(MYSQLI_ASSOC);
     }
