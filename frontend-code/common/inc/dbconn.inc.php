d3318ee4b13c3fb5051b31a19ea01ba551d4fc5b
Xinke Liu <xinkeliu365@gmail.com>
2024-10-08 15:43:04 +1030

    change the db to cloud

--- frontend-code/common/inc/dbconn.inc.php
+++ frontend-code/common/inc/dbconn.inc.php
@@ -1,15 +1,17 @@
 <?php
 
-define("DB_HOST", "localhost");
+define("DB_HOST", "database-9030.cho2aisawwwl.ap-southeast-2.rds.amazonaws.com");
 define("DB_NAME", "COMP9030");
-define("DB_USER", "dbadmin");
-define("DB_PASS", "");
+define("DB_USER", "admin");
+define("DB_PASS", "Test1234!");
 
-$conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306); // Port 3306 is used for MariaDB/MySQL
+
 if (!$conn) {
-    // Something went wrong...
+    // Error handling for the connection
     echo "Error: Unable to connect to database.<br>";
     echo "Debugging errno: " . mysqli_connect_errno() . "<br>";
     echo "Debugging error: " . mysqli_connect_error() . "<br>";
     exit;
 }
+?>
