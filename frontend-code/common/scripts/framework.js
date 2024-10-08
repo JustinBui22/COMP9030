4a31d514a2d738be974e5a94b2caf70a9e4d4422
Xinke Liu <xinkeliu365@gmail.com>
2024-10-08 23:08:31 +1030

    change the navigation link

--- frontend-code/common/scripts/framework.js
+++ frontend-code/common/scripts/framework.js
@@ -20,7 +20,7 @@ function loadContentBasedOnRole() {
 		// Patient-specific links
 		navItems = `
             <li><a href="../patient-module/dashboard/Dashboard.html" target="content-frame" data-initial="D">Dashboard</a></li>
-            <li><a href="../patient-module/dashboard/Patient.html" target="content-frame" data-initial="J">Exercise Tracker</a></li>
+            <li><a href="../patient-module/dashboard/Patient.php" target="content-frame" data-initial="J">Exercise Tracker</a></li>
             <li><a href="../patient-module/journal-entry/journalentry_page.html" target="content-frame" data-initial="J">Journal Entry</a></li>
             <li><a href="../patient-module/sleep-tracking/Home-Page/Home.html" target="content-frame" data-initial="S">Sleep Tracking</a></li>
             <li><a href="../patient-module/eating-habit/home.html" target="content-frame" data-initial="E">Eating Habit</a></li>
